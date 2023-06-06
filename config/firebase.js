e95eb58f3f8a7856c40fc70c1e064a63be857946
Arin Modi <arinmodi2306@gmail.com>
2023-06-06 11:23:11 +0530

    Small Config Changes

--- config/firebase.js
+++ config/firebase.js
@@ -9,9 +9,15 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+// put your firebase configurations below
 const firebaseConfig = {
-  // your firebase configurations
-};
+  apiKey: "AIzaSyDPPq0FoMmf-YyxHFJKUU7kqs-c1uTHn1Y",
+  authDomain: "file-transfer-18e8e.firebaseapp.com",
+  projectId: "file-transfer-18e8e",
+  storageBucket: "file-transfer-18e8e.appspot.com",
+  messagingSenderId: "103008805790",
+  appId: "1:103008805790:web:ab46239af480510957ac8f",
+  measurementId: "G-CZL0PQ4PNV"};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
