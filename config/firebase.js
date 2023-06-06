2c5642bebb78bf21708b0c97d5afa56ad2645799
Arin Modi <arinmodi2306@gmail.com>
2023-06-06 09:42:09 +0530

    Ready To Be Open

--- config/firebase.js
+++ config/firebase.js
@@ -2,6 +2,7 @@ const { initializeApp } = require("firebase/app");
 const { getStorage, uploadBytes } = require("firebase/storage")
 const admin = require("firebase-admin");
 
+// path to your service account key file
 const serviceAccount = require("../private/keyInfo.json");
 
 admin.initializeApp({
@@ -9,13 +10,7 @@ admin.initializeApp({
 });
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDPPq0FoMmf-YyxHFJKUU7kqs-c1uTHn1Y",
-    authDomain: "file-transfer-18e8e.firebaseapp.com",
-    projectId: "file-transfer-18e8e",
-    storageBucket: "file-transfer-18e8e.appspot.com",
-    messagingSenderId: "103008805790",
-    appId: "1:103008805790:web:ab46239af480510957ac8f",
-    measurementId: "G-CZL0PQ4PNV"
+  // your firebase configurations
 };
 
 // Initialize Firebase
